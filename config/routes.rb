0b8070b8e15e59249a7f628ff4165d9ec0c565ea
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-08-14 02:35:35 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
-  resources :products, only: [:show, :index, :new, :create]
+  resources :products, only: [:show, :index, :new, :create, :show]
   get '/products/:id/description', to: 'products#description'
   get '/products/:id/inventory', to: 'products#inventory'
-  get '/products/:id/data', to: 'products#data'
 end
